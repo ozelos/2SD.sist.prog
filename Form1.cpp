8e442064f7cbfa0aa5075496c183fd43cea9a488
ozelos <99196325+ozelos@users.noreply.github.com>
2024-12-20 17:47:53 +0200

    Update Form1.cpp

--- Form1.cpp
+++ Form1.cpp
@@ -1,3 +1,12 @@
-#include "pch.h"
-// #include "Form1.h"
+#include "Form1.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace StudentDataApp;
+
+[STAThread]
+void Main() {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew MainForm());
+}
